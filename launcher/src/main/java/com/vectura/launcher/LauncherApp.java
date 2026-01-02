d947201d3e1718fd6e3da9bd54cb022cac852faa
suspecttaco <adiel.gomez020601@gmail.com>
2026-01-02 01:26:19 -0700

    Add app icon to launcher window and include `app.png` resource file

--- launcher/src/main/java/com/vectura/launcher/LauncherApp.java
+++ launcher/src/main/java/com/vectura/launcher/LauncherApp.java
@@ -5,11 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class LauncherApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/selector.fxml"));
         Scene scene = new Scene(loader.load());
+        stage.getIcons().add(new javafx.scene.image.Image(Objects.requireNonNull(getClass().getResourceAsStream("/app.png"))));
         stage.setTitle("Vectura Suite - Selector");
         stage.setScene(scene);
         stage.setResizable(false);
